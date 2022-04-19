8c238dcff30dac027bcd0930a68c5d7c3d9e4fad
Cullen Middleton <cullen.middleton@yoco.com>
2022-04-19 12:38:31 +0200

    Q4

--- day-1/question-4.ts
+++ day-1/question-4.ts
@@ -3,26 +3,33 @@
 // Note: Year is an optional value
 // Add a return type
 
-const car1 = {
-  model: 'Skyline GTR V-Spec II',
-  make: 'Nissan',
+type Car = {
+  model: string;
+  make: string;
+  year?: number;
+  isCoupe?: boolean;
+};
+
+const car1: Car = {
+  model: "Skyline GTR V-Spec II",
+  make: "Nissan",
   year: 1999,
   isCoupe: true,
 };
-const car2 = {
-  model: 'Impreza WRX STi',
-  make: 'Subaru',
+const car2: Car = {
+  model: "Impreza WRX STi",
+  make: "Subaru",
   year: 1994,
 };
 
-function displayCar({ model, make, year, isCoupe }) {
+function displayCar({ model, make, year = -1, isCoupe = false }: Car): string {
   if (isCoupe) {
     return `The car is a ${make} ${model}, made in ${year}. Is a coupe.`;
   }
   return `The car is a ${make} ${model}, made in ${year}.`;
 }
 
-displayCar(car1);
-displayCar(car2);
+console.log(displayCar(car1));
+console.log(displayCar(car2));
 
 export {};
