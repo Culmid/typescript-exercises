83df46106c3b5513e0b8947dab09c9305c092bbf
Cullen Middleton <cullen.middleton@yoco.com>
2022-04-19 12:29:45 +0200

    Q3

--- day-1/question-3.ts
+++ day-1/question-3.ts
@@ -1,9 +1,16 @@
 // Question 3: Interface
 // Create an Interface for the object below.
 
-const person = {
-  firstName: 'Michael',
-  lastName: 'Scott',
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+  isAdmin: boolean;
+}
+
+const person: Person = {
+  firstName: "Michael",
+  lastName: "Scott",
   age: 40,
   isAdmin: true,
 };
