35745143617c5b1afa795794e055d378181c8de5
Cullen Middleton <cullen.middleton@yoco.com>
2022-04-19 12:27:04 +0200

    Q2

--- day-1/question-2.ts
+++ day-1/question-2.ts
@@ -1,10 +1,10 @@
 // Question 2: Function types
 // Add the correct types to the function below:
 
-function displayNameAndYear(name, year) {
+function displayNameAndYear(name: string, year: number) {
   console.log(`Hello ${name}, the year is ${year}.`);
 }
 
-displayNameAndYear('Michael', 2022);
+displayNameAndYear("Michael", 2022);
 
 export {};
